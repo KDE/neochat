d5847e797b232d1ceec2aaf36d70ab60101a952c
Black Hat <bhat@encom.eu.org>
2018-10-20 09:08:28 +0800

    Update notification for mac.

--- src/notifications/managermac.mm
+++ src/notifications/managermac.mm
@@ -7,27 +7,42 @@
 - (void)set_identityImage:(NSImage *)image;
 @end
 
-NotificationsManager::NotificationsManager(QObject *parent) : QObject(parent) {}
+NotificationsManager::NotificationsManager(QObject *parent): QObject(parent)
+{
 
-void NotificationsManager::postNotification(const QString &roomId, const QString &eventId,
-                                            const QString &roomName, const QString &senderName,
-                                            const QString &text, const QImage &icon) {
-  Q_UNUSED(roomId);
-  Q_UNUSED(eventId);
-  Q_UNUSED(icon);
-
-  NSUserNotification *notif = [[NSUserNotification alloc] init];
-
-  notif.title = roomName.toNSString();
-  notif.subtitle = QString("%1 sent a message").arg(senderName).toNSString();
-  notif.informativeText = text.toNSString();
-  notif.soundName = NSUserNotificationDefaultSoundName;
-
-  [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notif];
-  [notif autorelease];
 }
 
-// unused
-void NotificationsManager::actionInvoked(uint, QString) {}
+void
+NotificationsManager::postNotification(
+                const QString &roomId,
+                const QString &eventId,
+                const QString &roomName,
+                const QString &senderName,
+                const QString &text,
+                const QImage &icon)
+{
+    Q_UNUSED(roomId);
+    Q_UNUSED(eventId);
+    Q_UNUSED(icon);
 
-void NotificationsManager::notificationClosed(uint, uint) {}
+    NSUserNotification * notif = [[NSUserNotification alloc] init];
+
+    notif.title           = roomName.toNSString();
+    notif.subtitle        = QString("%1 sent a message").arg(senderName).toNSString();
+    notif.informativeText = text.toNSString();
+    notif.soundName       = NSUserNotificationDefaultSoundName;
+
+    [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification: notif];
+    [notif autorelease];
+}
+
+//unused
+void
+NotificationsManager::actionInvoked(uint, QString)
+{
+}
+
+void
+NotificationsManager::notificationClosed(uint, uint)
+{
+}
