KDE/neochat

commit 53397bf9701f84a0a3a60c0c876694c9e9631e4e

fix some clazy warning and deprecations

src/neochatroom.cpp
@@ -297,15 +297,15 @@ QString NeoChatRoom::eventToString(const RoomEvent &evt, Qt::TextFormat format, 
                 }
                 QString text {};
                 if (e.isRename()) {
-                    if (e.displayName().isEmpty())
+                    if (e.newDisplayName()->isEmpty())
                         text = tr("cleared their display name");
                     else
-                        text = tr("changed their display name to %1").arg(e.displayName().toHtmlEscaped());
+                        text = tr("changed their display name to %1").arg(e.newDisplayName()->toHtmlEscaped());
                 }
                 if (e.isAvatarUpdate()) {
                     if (!text.isEmpty())
                         text += " and ";
-                    if (e.avatarUrl().isEmpty())
+                    if (e.newAvatarUrl()->isEmpty())
                         text += tr("cleared their avatar");
                     else if (e.prevContent()->avatarUrl->isEmpty())
                         text += tr("set an avatar");

src/controller.h
@@ -47,7 +47,7 @@ public:
     Q_INVOKABLE void loginWithCredentials(QString, QString, QString, QString);
     Q_INVOKABLE void loginWithAccessToken(QString, QString, QString, QString);
 
-    Q_INVOKABLE void changePassword(Connection *connection, const QString &currentPassword, const QString &newPassword);
+    Q_INVOKABLE void changePassword(Quotient::Connection *connection, const QString &currentPassword, const QString &newPassword);
 
     int accountCount() const;
 
@@ -104,7 +104,7 @@ Q_SIGNALS:
     void connectionChanged();
     void isOnlineChanged();
     void aboutDataChanged();
-    void passwordStatus(PasswordStatus status);
+    void passwordStatus(Controller::PasswordStatus status);
 
 public Q_SLOTS:
     void logout(Quotient::Connection *conn);

commit d9f0ec8adaf99d674e315674d2b8e977a3435ec8

Fix segfault on login caused by 53397bf9701f84a0a3a60c0c876694c9e9631e4e
newAvatarUrl() returns an Omittable<QUrl> instead of a QUrl that may be
empty. When the Omittable is empty, dereferencing it returns a nullptr
and calling something on that segfaults

src/neochatroom.cpp
@@ -305,7 +305,7 @@ QString NeoChatRoom::eventToString(const RoomEvent &evt, Qt::TextFormat format, 
                 if (e.isAvatarUpdate()) {
                     if (!text.isEmpty())
                         text += " and ";
-                    if (e.newAvatarUrl()->isEmpty())
+                    if (!e.newAvatarUrl())
                         text += tr("cleared their avatar");
                     else if (e.prevContent()->avatarUrl->isEmpty())
                         text += tr("set an avatar");