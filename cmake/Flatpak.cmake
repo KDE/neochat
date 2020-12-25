6351454759e0a9cacaf04e993c7a1cbceed4a165
Tobias Fella <fella@posteo.de>
2020-12-25 17:59:50 +0100

    CMake Cleanup

--- cmake/Flatpak.cmake
+++ cmake/Flatpak.cmake
@@ -1,8 +1,3 @@
-if(NOT NEOCHAT_FLATPAK)
-    # Only include this if we build a Flatpak
-    return()
-endif()
-
 include(GNUInstallDirs)
 
 # Include FontConfig config which uses the Emoji One font from the
