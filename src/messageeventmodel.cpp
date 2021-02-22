bb3b3bc0883333ae9e99b280eb5f98c5ff19a927
Carl Schwan <carl@carlschwan.eu>
2021-02-22 19:09:56 +0000

    Input field fixes
    * Message with multiple mentions are not broken in IRC. Fix #267
    * Editing a message won't remove mentions anymore

--- src/messageeventmodel.cpp
+++ src/messageeventmodel.cpp
@@ -46,6 +46,7 @@ QHash<int, QByteArray> MessageEventModel::roleNames() const
     roles[ShowSectionRole] = "showSection";
     roles[ReactionRole] = "reaction";
     roles[IsEditedRole] = "isEdited";
+    roles[FormattedBodyRole] = "formattedBody";
     return roles;
 }
 
@@ -332,6 +333,16 @@ QVariant MessageEventModel::data(const QModelIndex &idx, int role) const
         }
 
         return m_currentRoom->eventToString(evt, Qt::RichText);
+    }
+
+    if (role == FormattedBodyRole) {
+        if (auto e = eventCast<const RoomMessageEvent>(&evt)) {
+            if (e->hasTextContent() && e->mimeType().name() != "text/plain") {
+                return static_cast<const Quotient::EventContent::TextContent *>(e->content())->body;
+            }
+        }
+
+        return {};
     }
 
     if (role == MessageRole) {
