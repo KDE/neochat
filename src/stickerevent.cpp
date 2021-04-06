57d678d4d82586569e5ae27cad93f964b214204f
Tobias Fella <fella@posteo.de>
2021-04-06 15:56:02 +0200

    Fix even more copyright headers

--- src/stickerevent.cpp
+++ src/stickerevent.cpp
@@ -1,4 +1,4 @@
-// SDPX-FileCopyrightText: 2020 Carl Schwan <carlschwan@kde.org>
+// SPDX-FileCopyrightText: 2020 Carl Schwan <carlschwan@kde.org>
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 #include "stickerevent.h"
