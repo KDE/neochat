592c33ed8b221ce690eca890cc79f92ffb0f7e63
Tobias Fella <fella@posteo.de>
2021-04-06 15:56:37 +0200

    Add missing license header

--- src/chatdocumenthandler.cpp
+++ src/chatdocumenthandler.cpp
@@ -1,3 +1,9 @@
+/**
+ * SPDX-FileCopyrightText: 2020 Carl Schwan <carlschwan@kde.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #include "chatdocumenthandler.h"
 
 #include <QQmlFile>
