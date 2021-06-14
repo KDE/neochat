8c1129f88ed420df31061c19317e9439fecbd71d
Carl Schwan <carl@carlschwan.eu>
2021-06-14 16:18:20 +0200

    Add missing license

--- src/colorschemer.cpp
+++ src/colorschemer.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2021 Carl Schwan <carlschwan@kde.org>
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 #include <KColorSchemeManager>
 #include <QAbstractItemModel>
 
--- src/colorschemer.h
+++ src/colorschemer.h
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2021 Carl Schwan <carlschwan@kde.org>
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 #pragma once
 
 #include <QObject>
