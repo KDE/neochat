a6dd5b9a573afcc2ae127f55e52fc5fdbd079ac1
Tobias Fella <fella@posteo.de>
2021-10-16 20:45:14 +0200

    Escape html before processing text to be sent

--- src/actionshandler.cpp
+++ src/actionshandler.cpp
@@ -106,7 +106,7 @@ void ActionsHandler::postMessage(const QString &text,
                                  CustomEmojiModel *cem)
 {
     QString rawText = text;
-    QString cleanedText = text;
+    QString cleanedText = text.toHtmlEscaped();
 
     auto preprocess = [cem](const QString &it) -> QString {
         if (cem == nullptr) {
