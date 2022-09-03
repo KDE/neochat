14e57e78333d96704bf7ebc8d97dd350654e7d89
James Graham <james.h.graham@protonmail.com>
2022-09-03 14:33:58 +0000

    Fix show author after state regression
    The avatar should be shown after a state message
    
    Fixes network/neochat#553

--- src/messageeventmodel.cpp
+++ src/messageeventmodel.cpp
@@ -715,7 +715,8 @@ QVariant MessageEventModel::data(const QModelIndex &idx, int role) const
         for (auto r = row + 1; r < rowCount(); ++r) {
             auto i = index(r);
             if (data(i, SpecialMarksRole) != EventStatus::Hidden) {
-                return data(i, AuthorRole) != data(idx, AuthorRole) || data(i, TimeRole).toDateTime().msecsTo(data(idx, TimeRole).toDateTime()) > 600000
+                return data(i, AuthorRole) != data(idx, AuthorRole) || data(i, EventTypeRole) == "state"
+                    || data(i, TimeRole).toDateTime().msecsTo(data(idx, TimeRole).toDateTime()) > 600000
                     || data(i, TimeRole).toDateTime().toLocalTime().date().day() != data(idx, TimeRole).toDateTime().toLocalTime().date().day();
             }
         }
