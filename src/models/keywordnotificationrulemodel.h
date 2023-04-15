d750263d3956ef20bb8fec69b688edb27a871f13
James Graham <james.h.graham@protonmail.com>
2023-04-15 08:32:35 +0000

    Document keywordnotificationrulemodel

--- src/models/keywordnotificationrulemodel.h
+++ src/models/keywordnotificationrulemodel.h
@@ -7,23 +7,54 @@
 
 #include <QAbstractListModel>
 
+/**
+ * @class KeywordNotificationRuleModel
+ *
+ * This class defines the model for managing notification push rule keywords.
+ */
 class KeywordNotificationRuleModel : public QAbstractListModel
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Defines the model roles.
+     */
     enum EventRoles {
-        NameRole = Qt::DisplayRole,
+        NameRole = Qt::DisplayRole, /**< The push rule keyword. */
     };
 
     KeywordNotificationRuleModel(QObject *parent = nullptr);
 
+    /**
+     * @brief Get the given role value at the given index.
+     *
+     * @sa QAbstractItemModel::data
+     */
     [[nodiscard]] QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+    /**
+     * @brief Number of rows in the model.
+     *
+     * @sa QAbstractItemModel::rowCount
+     */
     [[nodiscard]] int rowCount(const QModelIndex &parent = QModelIndex()) const override;
 
+    /**
+     * @brief Returns a mapping from Role enum values to role names.
+     *
+     * @sa EventRoles, QAbstractItemModel::roleNames()
+     */
     [[nodiscard]] QHash<int, QByteArray> roleNames() const override;
 
+    /**
+     * @brief Add a new keyword to the model.
+     */
     Q_INVOKABLE void addKeyword(const QString &keyword);
+
+    /**
+     * @brief Remove a keyword from the model.
+     */
     Q_INVOKABLE void removeKeywordAtIndex(int index);
 
 private Q_SLOTS:
