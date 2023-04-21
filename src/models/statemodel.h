3f572c79fafae8dc65b30b0707b6230e42844395
James Graham <james.h.graham@protonmail.com>
2023-04-21 17:03:03 +0100

    Document statemodel

--- src/models/statemodel.h
+++ src/models/statemodel.h
@@ -7,27 +7,56 @@
 
 #include "neochatroom.h"
 
+/**
+ * @class StateModel
+ *
+ * This class defines the model for visualising the state events in a room.
+ */
 class StateModel : public QAbstractListModel
 {
     Q_OBJECT
 
+    /**
+     * @brief The current room that the model is getting its state events from.
+     */
     Q_PROPERTY(NeoChatRoom *room READ room WRITE setRoom NOTIFY roomChanged)
 
 public:
+    /**
+     * @brief Defines the model roles.
+     */
     enum Roles {
-        TypeRole,
-        StateKeyRole,
-        SourceRole,
+        TypeRole, /**< The type of the state event. */
+        StateKeyRole, /**< The state key of the state event. */
+        SourceRole, /**< The full event source JSON. */
     };
     Q_ENUM(Roles);
 
     StateModel(QObject *parent = nullptr);
-    QHash<int, QByteArray> roleNames() const override;
-    QVariant data(const QModelIndex &index, int role) const override;
-    int rowCount(const QModelIndex &parent) const override;
 
     NeoChatRoom *room() const;
     void setRoom(NeoChatRoom *room);
+
+    /**
+     * @brief Get the given role value at the given index.
+     *
+     * @sa QAbstractItemModel::data
+     */
+    QVariant data(const QModelIndex &index, int role) const override;
+
+    /**
+     * @brief Number of rows in the model.
+     *
+     * @sa QAbstractItemModel::rowCount
+     */
+    int rowCount(const QModelIndex &parent) const override;
+
+    /**
+     * @brief Returns a mapping from Role enum values to role names.
+     *
+     * @sa Roles, QAbstractItemModel::roleNames()
+     */
+    QHash<int, QByteArray> roleNames() const override;
 
 Q_SIGNALS:
     void roomChanged();
