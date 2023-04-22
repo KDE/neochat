0496f6552a502559d4d75a5ea1db4ecf1d61fa9a
James Graham <james.h.graham@protonmail.com>
2023-04-22 11:38:35 +0100

    Document blurhashimageprovider

--- src/blurhashimageprovider.h
+++ src/blurhashimageprovider.h
@@ -5,9 +5,22 @@
 
 #include <QQuickImageProvider>
 
+/**
+ * @class BlurhashImageProvider
+ *
+ * A QQuickImageProvider for blurhashes.
+ *
+ * @sa QQuickImageProvider
+ */
 class BlurhashImageProvider : public QQuickImageProvider
 {
 public:
     BlurhashImageProvider();
+
+    /**
+     * @brief Return an image for a given ID.
+     *
+     * @sa QQuickImageProvider::requestImage
+     */
     QImage requestImage(const QString &id, QSize *size, const QSize &requestedSize) override;
 };
