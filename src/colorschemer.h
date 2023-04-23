25754bbe22bb444ca6c7def419de37bb6a7a9512
James Graham <james.h.graham@protonmail.com>
2023-04-23 12:44:52 +0000

    Document colorschemer

--- src/colorschemer.h
+++ src/colorschemer.h
@@ -8,18 +8,53 @@
 class QAbstractItemModel;
 class KColorSchemeManager;
 
+/**
+ * @class ColorSchemer
+ *
+ * A class to provide a wrapper around KColorSchemeManager to make it available in
+ * QML.
+ *
+ * @sa KColorSchemeManager
+ */
 class ColorSchemer : public QObject
 {
     Q_OBJECT
+
+    /**
+     * @brief A QAbstractItemModel of all available color schemes.
+     *
+     * @sa QAbstractItemModel
+     */
     Q_PROPERTY(QAbstractItemModel *model READ model CONSTANT)
+
 public:
     ColorSchemer(QObject *parent = nullptr);
     ~ColorSchemer();
 
     QAbstractItemModel *model() const;
+
+    /**
+     * @brief Activates the KColorScheme identified by the provided index.
+     *
+     * @sa KColorScheme
+     */
     Q_INVOKABLE void apply(int idx);
+
+    /**
+     * @brief Activates the KColorScheme with the given name.
+     *
+     * @sa KColorScheme
+     */
     Q_INVOKABLE void apply(const QString &name);
+
+    /**
+     * @brief Returns the index for the scheme with the given name.
+     */
     Q_INVOKABLE int indexForScheme(const QString &name) const;
+
+    /**
+     * @brief Returns the name for the scheme with the given index.
+     */
     Q_INVOKABLE QString nameForIndex(int index) const;
 
 private:
