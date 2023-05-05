65d919876c489baf77e3189239ffc453508c6eae
James Graham <james.h.graham@protonmail.com>
2023-05-05 16:53:38 +0000

    Document trayicon_sni

--- src/trayicon_sni.h
+++ src/trayicon_sni.h
@@ -5,15 +5,32 @@
 
 #include <KStatusNotifierItem>
 
+/**
+ * @class TrayIcon
+ *
+ * A class inheriting KStatusNotifierItem to provide a tray icon.
+ *
+ * @sa KStatusNotifierItem
+ */
 class TrayIcon : public KStatusNotifierItem
 {
     Q_OBJECT
 public:
     TrayIcon(QObject *parent = nullptr);
 
+    /**
+     * @brief Show the tray icon.
+     */
     void show();
+
+    /**
+     * @brief Hide the tray icon.
+     */
     void hide();
 
 Q_SIGNALS:
+    /**
+     * @brief Triggered when the system tray icon is clicked to request NeoChat be shown.
+     */
     void showWindow();
 };
