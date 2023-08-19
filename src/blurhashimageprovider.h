e2b29f27af442a19134f1cd39ccdec333697ef8e
Joshua Goins <josh@redstrate.com>
2023-08-19 14:06:11 -0400

    Update blurhash implementation, and add async image provider
    This makes it slightly faster to generate blurhash images. The default
    resolution for blurhashes has been reduced to 64x64 as well.

--- src/blurhashimageprovider.h
+++ src/blurhashimageprovider.h
@@ -1,26 +1,25 @@
-// SPDX-FileCopyrightText: 2021 Tobias Fella <tobias.fella@kde.org>
+// SPDX-FileCopyrightText: 2021 Tobias Fella <fella@posteo.de>
 // SPDX-License-Identifier: LGPL-2.0-or-later
 
 #pragma once
 
-#include <QQuickImageProvider>
+#include <QQuickAsyncImageProvider>
+#include <QThreadPool>
 
-/**
- * @class BlurhashImageProvider
- *
- * A QQuickImageProvider for blurhashes.
- *
- * @sa QQuickImageProvider
- */
-class BlurhashImageProvider : public QQuickImageProvider
+class AsyncImageResponse : public QQuickImageResponse
 {
 public:
-    BlurhashImageProvider();
+    AsyncImageResponse(const QString &id, const QSize &requestedSize, QThreadPool *pool);
+    void handleDone(QImage image);
+    QQuickTextureFactory *textureFactory() const override;
+    QImage m_image;
+};
 
-    /**
-     * @brief Return an image for a given ID.
-     *
-     * @sa QQuickImageProvider::requestImage
-     */
-    QImage requestImage(const QString &id, QSize *size, const QSize &requestedSize) override;
+class BlurhashImageProvider : public QQuickAsyncImageProvider
+{
+public:
+    QQuickImageResponse *requestImageResponse(const QString &id, const QSize &requestedSize) override;
+
+private:
+    QThreadPool pool;
 };
