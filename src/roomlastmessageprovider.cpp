e44cd405b7e5576030de4e3763b731c114b5c126
Carl Schwan <carl@carlschwan.eu>
2023-09-28 10:31:14 +0200

    Fix import name

--- src/roomlastmessageprovider.cpp
+++ src/roomlastmessageprovider.cpp
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2023 Carl Schwan <carl@carlschwan.eu>
 // SPDX-License-Identifier: LGPL-2.0-or-later
 
-#include "roomlastmessagestate.h"
+#include "roomlastmessageprovider.h"
 
 using namespace Qt::Literals::StringLiterals;
 
