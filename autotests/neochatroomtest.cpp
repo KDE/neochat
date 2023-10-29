f3c4d9449a2921b6614d4957b08f2f2c569923a3
James Graham <james.h.graham@protonmail.com>
2023-10-29 13:12:05 +0000

    Move remaining tests to external Json files
    Title

--- autotests/neochatroomtest.cpp
+++ autotests/neochatroomtest.cpp
@@ -42,101 +42,18 @@ void NeoChatRoomTest::initTestCase()
     connection = Connection::makeMockConnection(QStringLiteral("@bob:kde.org"));
     room = new TestRoom(connection, QStringLiteral("#myroom:kde.org"), JoinState::Join);
 
-    auto json = QJsonDocument::fromJson(R"EVENT({
-  "account_data": {
-    "events": [
-      {
-        "content": {
-          "tags": {
-            "u.work": {
-              "order": 0.9
-            }
-          }
-        },
-        "type": "m.tag"
-      },
-      {
-        "content": {
-          "custom_config_key": "custom_config_value"
-        },
-        "type": "org.example.custom.room.config"
-      }
-    ]
-  },
-  "ephemeral": {
-    "events": [
-      {
-        "content": {
-          "user_ids": [
-            "@alice:matrix.org",
-            "@bob:example.com"
-          ]
-        },
-        "room_id": "!jEsUZKDJdhlrceRyVU:example.org",
-        "type": "m.typing"
-      }
-    ]
-  },
-  "state": {
-    "events": [
-      {
-        "content": {
-          "avatar_url": "mxc://example.org/SEsfnsuifSDFSSEF",
-          "displayname": "Alice Margatroid",
-          "membership": "join",
-          "reason": "Looking for support"
-        },
-        "event_id": "$143273582443PhrSn:example.org",
-        "origin_server_ts": 1432735824653,
-        "room_id": "!jEsUZKDJdhlrceRyVU:example.org",
-        "sender": "@example:example.org",
-        "state_key": "@alice:example.org",
-        "type": "m.room.member",
-        "unsigned": {
-          "age": 1234
-        }
-      }
-    ]
-  },
-  "summary": {
-    "m.heroes": [
-      "@alice:example.com",
-      "@bob:example.com"
-    ],
-    "m.invited_member_count": 0,
-    "m.joined_member_count": 2
-  },
-  "timeline": {
-    "events": [
-      {
-        "content": {
-          "body": "This is an **example** text message",
-          "format": "org.matrix.custom.html",
-          "formatted_body": "<b>This is an example text message</b>",
-          "msgtype": "m.text"
-        },
-        "event_id": "$143273582443PhrSn:example.org",
-        "origin_server_ts": 1432735824654,
-        "room_id": "!jEsUZKDJdhlrceRyVU:example.org",
-        "sender": "@example:example.org",
-        "type": "m.room.message",
-        "unsigned": {
-          "age": 1235
-        }
-      }
-    ],
-    "limited": true,
-    "prev_batch": "t34-23535_0_0"
-  }
-})EVENT");
-    SyncRoomData roomData(QStringLiteral("@bob:kde.org"), JoinState::Join, json.object());
+    QFile testMinSyncFile;
+    testMinSyncFile.setFileName(QLatin1String(DATA_DIR) + u'/' + QLatin1String("test-min-sync.json"));
+    testMinSyncFile.open(QIODevice::ReadOnly);
+    const auto testMinSyncJson = QJsonDocument::fromJson(testMinSyncFile.readAll());
+    SyncRoomData roomData(QStringLiteral("@bob:kde.org"), JoinState::Join, testMinSyncJson.object());
     room->update(std::move(roomData));
 }
 
 void NeoChatRoomTest::subtitleTextTest()
 {
     QCOMPARE(room->timelineSize(), 1);
-    QCOMPARE(room->lastEventToString(), QStringLiteral("@example:example.org: This is an example text message"));
+    QCOMPARE(room->lastEventToString(), QStringLiteral("@example:example.org: This is an example\ntext message"));
 }
 
 void NeoChatRoomTest::eventTest()
