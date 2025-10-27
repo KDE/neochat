8d2c133e6310687fb48b409a5bc545a30f21b555
Joshua Goins <josh@redstrate.com>
2025-10-27 17:07:25 -0400

    Add a new function to check supported Matrix spec in QML

--- src/libneochat/neochatconnection.cpp
+++ src/libneochat/neochatconnection.cpp
@@ -574,4 +574,9 @@ bool NeoChatConnection::isVerifiedSession() const
     return isVerifiedDevice(userId(), deviceId());
 }
 
+void NeoChatConnection::reportUser(const QString &userId, const QString &reason)
+{
+    callApi<NeochatReportUserJob>(userId, reason);
+}
+
 #include "moc_neochatconnection.cpp"
