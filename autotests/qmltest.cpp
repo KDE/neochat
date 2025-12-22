8a296886a2b1146bbba61cfc0f16cf8624887d0f
James Graham <james.h.graham@protonmail.com>
2025-12-22 13:39:28 +0000

    Add qml test for chatdocumenthandler

---
+++ autotests/qmltest.cpp
@@ -0,0 +1,9 @@
+/*
+ *  SPDX-FileCopyrightText: 2020 Arjen Hiemstra <ahiemstra@heimr.nl>
+ *
+ *  SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include <quicktest.h>
+
+QUICK_TEST_MAIN(Kirigami)
