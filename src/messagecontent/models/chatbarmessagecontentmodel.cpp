b4ea023d708c6291219c11530f290ac6fa6c0532
Tobias Fella <tobias.fella@kde.org>
2026-04-08 13:43:10 +0200

    Fix icon for files in chatbar

--- src/messagecontent/models/chatbarmessagecontentmodel.cpp
+++ src/messagecontent/models/chatbarmessagecontentmodel.cpp
@@ -490,7 +490,7 @@ QVariantMap ChatBarMessageContentModel::attributesForFile(const QUrl &path)
         {"filename"_L1, path.fileName()},
         {"source"_L1, path},
         {"animated"_L1, false},
-        {"mimeIcon"_L1, mime.name()},
+        {"mimeIcon"_L1, mime.iconName()},
         {"size"_L1, fileInfo.size()},
     };
 }
