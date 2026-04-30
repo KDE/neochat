KDE/neochat

commit d5b54a0e61f2a257763041f01b799d1cd0998cb7

Only start spellchecking after a short delay
This is to prevent users from seeing it highlight words they're typing
in real-time.

src/messagecontent/TextComponent.qml
@@ -119,6 +119,19 @@ TextEdit {
     } else {
         (QQC2.ApplicationWindow.window as Main).hoverLinkIndicator.text = "";
     }
+    onTextEdited: {
+        spellcheckHighlighterInstantiator.object.active = false;
+        spellingTimer.restart();
+    }
+
+    // Delay checking spelling so it doesn't highlight words the user is currently typing
+    Timer {
+        id: spellingTimer
+
+        interval: 1000
+
+        onTriggered: spellcheckHighlighterInstantiator.object.active = true
+    }
 
     QQC2.Label {
         id: placeholder

commit e342333d42fa89b4aabdcd98b2f825d06885a1c0

Only spell-check words if turned on again
Fixes a regression in d5b54a0e61f2a257763041f01b799d1cd0998cb7 where I
accidentally always enabled spell-checking even if the user turned it
off.

BUG: 519565
FIXED-IN: 26.04.1

src/messagecontent/TextComponent.qml
@@ -114,7 +114,7 @@ TextEdit {
 
         interval: 1000
 
-        onTriggered: spellcheckHighlighterInstantiator.object.highlighter.active = true
+        onTriggered: spellcheckHighlighterInstantiator.object.highlighter.active = spellcheckHighlighterInstantiator.object.settings.checkerEnabledByDefault
     }
 
     QQC2.Label {
@@ -178,7 +178,6 @@ TextEdit {
         QtObject {
             readonly property Sonnet.Settings settings: Sonnet.Settings {}
             readonly property Sonnet.SpellcheckHighlighter highlighter: Sonnet.SpellcheckHighlighter {
-                active: settings.checkerEnabledByDefault
                 document: root.textDocument
                 cursorPosition: root.cursorPosition
                 selectionStart: root.selectionStart