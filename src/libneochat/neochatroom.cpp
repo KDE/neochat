e5a782386869170b1e91b78dc128da82ba04e3c2
Joshua Goins <josh@redstrate.com>
2026-05-09 10:59:30 -0400

    Wait until encryption is set up before decrypting pinned message
    See https://github.com/quotient-im/libQuotient/pull/977 for the crash
    related to this issue.
    
    (cherry picked from commit 27334d921b8327fc03d3250e2d2ae458e572125b)

--- src/libneochat/neochatroom.cpp
+++ src/libneochat/neochatroom.cpp
@@ -131,9 +131,15 @@ NeoChatRoom::NeoChatRoom(Connection *c, QString roomId, JoinState joinState)
         this,
         &Room::baseStateLoaded,
         this,
-        [this]() {
+        [this, connection] {
             updatePushNotificationState(u"m.push_rules"_s);
-            loadPinnedMessage();
+
+            // We need to wait until encryption is set up, otherwise the event isn't decrypted.
+            if (!connection->encryptionEnabled()) {
+                connect(connection, &Connection::ready, this, &NeoChatRoom::loadPinnedMessage, Qt::SingleShotConnection);
+            } else {
+                loadPinnedMessage();
+            }
 
             Q_EMIT canEncryptRoomChanged();
             Q_EMIT inviteTimestampChanged();
