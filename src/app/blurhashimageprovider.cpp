a7228c73fc03dcf74ceb51ea189db709af164c49
Tobias Fella <tobias.fella@kde.org>
2026-07-04 21:08:15 +0200

    Minor cleanup in BlurhashImageProvider

--- src/app/blurhashimageprovider.cpp
+++ src/app/blurhashimageprovider.cpp
@@ -9,14 +9,14 @@
 #include "blurhash.h"
 
 BlurhashImageProvider::BlurhashImageProvider()
-    : QQuickImageProvider(QQuickImageProvider::Image)
+    : QQuickImageProvider(Image)
 {
 }
 
 QImage BlurhashImageProvider::requestImage(const QString &id, QSize *size, const QSize &requestedSize)
 {
     if (id.isEmpty()) {
-        return QImage();
+        return {};
     }
     *size = requestedSize;
     if (size->width() == -1) {
@@ -25,7 +25,7 @@ QImage BlurhashImageProvider::requestImage(const QString &id, QSize *size, const
     if (size->height() == -1) {
         size->setHeight(256);
     }
-    auto data = decode(QUrl::fromPercentEncoding(id.toLatin1()).toLatin1().data(), size->width(), size->height(), 1, 3);
+    const auto data = decode(QUrl::fromPercentEncoding(id.toLatin1()).toLatin1().data(), size->width(), size->height(), 1, 3);
     QImage image(data, size->width(), size->height(), size->width() * 3, QImage::Format_RGB888, free, data);
     return image;
 }
