61e4693906dfd214f4221e1c40e7bb96f5cb79d4
Tobias Fella <tobias.fella@kde.org>
2026-09-13 00:27:36 +0200

    Improve robustness of DevicesModel
    - Always clear the model before loading
    - Discard data when connection changes while request is in flight
    - Check for nullptr connection in before doing things

--- src/settings/models/devicesmodel.cpp
+++ src/settings/models/devicesmodel.cpp
@@ -22,13 +22,19 @@ DevicesModel::DevicesModel(QObject *parent)
 
 void DevicesModel::fetchDevices()
 {
+    beginResetModel();
+    m_devices.clear();
+    endResetModel();
+
     if (!m_connection) {
-        beginResetModel();
-        m_devices.clear();
-        endResetModel();
         return;
     }
-    m_connection->callApi<GetDevicesJob>().onResult(this, [this](const auto &job) {
+
+    auto connection = m_connection;
+    m_connection->callApi<GetDevicesJob>().onResult(this, [this, connection](const auto &job) {
+        if (connection != m_connection) {
+            return;
+        }
         beginResetModel();
         m_devices = job->devices();
         endResetModel();
@@ -38,7 +44,13 @@ void DevicesModel::fetchDevices()
 
 QVariant DevicesModel::data(const QModelIndex &index, int role) const
 {
-    if (index.row() < 0 || index.row() >= rowCount(QModelIndex())) {
+    if (!checkIndex(index, CheckIndexOption::IndexIsValid | CheckIndexOption::ParentIsInvalid)) {
+        qWarning() << Q_FUNC_INFO << "called with invalid index" << index << role;
+        return {};
+    }
+
+    if (!m_connection) {
+        qWarning() << Q_FUNC_INFO << "caleld nullptr connection";
         return {};
     }
 
@@ -78,7 +90,9 @@ QVariant DevicesModel::data(const QModelIndex &index, int role) const
 
 int DevicesModel::rowCount(const QModelIndex &parent) const
 {
-    Q_UNUSED(parent);
+    if (parent.isValid()) {
+        return 0;
+    }
     return m_devices.size();
 }
 
@@ -109,7 +123,18 @@ std::optional<int> DevicesModel::findDevice(const QString &deviceId) const
 
 void DevicesModel::logout(const QString &deviceId, const QString &password)
 {
-    auto onSuccess = [this, deviceId] {
+    if (!m_connection) {
+        qWarning() << Q_FUNC_INFO << "called nullptr connection";
+        return;
+    }
+
+    // Need to store the connection since it could be switched while the first job is running
+    auto connection = m_connection;
+
+    auto onSuccess = [this, connection, deviceId] {
+        if (connection != m_connection) {
+            return;
+        }
         const auto maybeIndex = findDevice(deviceId);
         if (!maybeIndex.has_value()) {
             return;
@@ -120,39 +145,54 @@ void DevicesModel::logout(const QString &deviceId, const QString &password)
         endRemoveRows();
         Q_EMIT countChanged();
     };
-    m_connection->callApi<DeleteDeviceJob>(deviceId).then(this, onSuccess, [password, this, deviceId, onSuccess](const auto &job) {
+    connection->callApi<DeleteDeviceJob>(deviceId).then(this, onSuccess, [password, connection, deviceId, onSuccess, this](const auto &job) {
+        if (!connection) {
+            return;
+        }
         QJsonObject replyData = job->jsonData();
         AuthenticationData authData;
         authData.session = replyData["session"_L1].toString();
         authData.authInfo["password"_L1] = password;
         authData.type = "m.login.password"_L1;
-        authData.authInfo["identifier"_L1] = QJsonObject{{"type"_L1, "m.id.user"_L1}, {"user"_L1, m_connection->user()->id()}};
-        m_connection->callApi<DeleteDeviceJob>(deviceId, authData).onResult(this, onSuccess);
+        authData.authInfo["identifier"_L1] = QJsonObject{{"type"_L1, "m.id.user"_L1}, {"user"_L1, connection->user()->id()}};
+        connection->callApi<DeleteDeviceJob>(deviceId, authData).onResult(this, onSuccess);
     });
 }
 
 void DevicesModel::setName(const QString &deviceId, const QString &name)
 {
+    if (!m_connection) {
+        qWarning() << Q_FUNC_INFO << "called with nullptr connection";
+        return;
+    }
+
     const auto maybeIndex = findDevice(deviceId);
     if (!maybeIndex.has_value()) {
         return;
     }
     const auto index = *maybeIndex;
 
-    auto job = m_connection->callApi<UpdateDeviceJob>(deviceId, name);
-    QString oldName = m_devices[index].displayName;
-    beginResetModel();
+    auto connection = m_connection;
+    auto job = connection->callApi<UpdateDeviceJob>(deviceId, name);
+    const auto oldName = m_devices[index].displayName;
     m_devices[index].displayName = name;
-    endResetModel();
-    connect(job, &BaseJob::failure, this, [this, oldName, deviceId] {
+    Q_EMIT dataChanged(this->index(index, 0), this->index(index, 0), {DisplayName});
+    connect(job, &BaseJob::failure, this, [this, connection, oldName, deviceId, name] {
+        if (connection != m_connection) {
+            return;
+        }
         const auto maybeIndex = findDevice(deviceId);
         if (!maybeIndex.has_value()) {
             return;
         }
+
         const auto index = *maybeIndex;
-        beginResetModel();
+        // If the name has meanwhile changed, don't reset it
+        if (name != data(this->index(index, 0), DisplayName)) {
+            return;
+        }
         m_devices[index].displayName = oldName;
-        endResetModel();
+        Q_EMIT dataChanged(this->index(index, 0), this->index(index, 0), {DisplayName});
     });
 }
 
@@ -168,17 +208,20 @@ void DevicesModel::setConnection(NeoChatConnection *connection)
     }
     m_connection = connection;
     Q_EMIT connectionChanged();
+
     fetchDevices();
 
-    connect(m_connection, &Connection::sessionVerified, this, [this](const QString &userId, const QString &deviceId) {
-        Q_UNUSED(deviceId);
-        if (userId == m_connection->userId()) {
+    if (m_connection) {
+        connect(m_connection, &Connection::sessionVerified, this, [this](const QString &userId, const QString &deviceId) {
+            Q_UNUSED(deviceId);
+            if (userId == m_connection->userId()) {
+                fetchDevices();
+            }
+        });
+        connect(m_connection, &Connection::finishedQueryingKeys, this, [this]() {
             fetchDevices();
-        }
-    });
-    connect(m_connection, &Connection::finishedQueryingKeys, this, [this]() {
-        fetchDevices();
-    });
+        });
+    }
 }
 
 #include "moc_devicesmodel.cpp"
