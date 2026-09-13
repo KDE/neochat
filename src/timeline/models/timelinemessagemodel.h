88307159c46ee1ed3560d6d509c53912c7c77239
Tobias Fella <tobias.fella@kde.org>
2026-09-13 21:44:06 +0200

    Remove unused variable

--- src/timeline/models/timelinemessagemodel.h
+++ src/timeline/models/timelinemessagemodel.h
@@ -47,8 +47,6 @@ private:
 
     std::optional<std::reference_wrapper<const Quotient::RoomEvent>> getEventForIndex(QModelIndex index) const override;
 
-    int rowBelowInserted = -1;
-
     int timelineServerIndex() const override;
 
     // Hack to ensure that we don't call endInsertRows when we haven't called beginInsertRows
